3a96b11c811f72e9262438f47f7c967f7ca8d13b
eyob j <jemijobs22@gmail.com>
2024-11-12 09:26:36 +0300

    feat: add background & improve animation

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import GUI from 'lil-gui'
 import gsap from 'gsap'
-
+import { Sky } from 'three/addons/objects/Sky.js'
 import fireworkVertexShader from './shaders/firework/vertex.glsl?raw'
 import fireworkFragmentShader from './shaders/firework/fragment.glsl?raw'
 
@@ -95,6 +95,8 @@ function createFireWork(count, size, position, texture, radius, color){
 
     const positionArray = new Float32Array(count * 3)
     const sizesArray = new Float32Array(count)
+    const timeMultiplyerArray = new Float32Array(count)
+
 
 
     for ( let i = 0; i < count; i++){
@@ -114,12 +116,17 @@ function createFireWork(count, size, position, texture, radius, color){
         positionArray[i3 + 2] = position.z
 
         sizesArray[i] = Math.random()
+        
+        timeMultiplyerArray[i] = 1. + Math.random()
 
     }
 
     const geometry = new THREE.BufferGeometry();
     geometry.setAttribute( 'position', new THREE.Float32BufferAttribute(positionArray, 3))
     geometry.setAttribute( 'aSizes', new THREE.Float32BufferAttribute(sizesArray, 1))
+    geometry.setAttribute( 'aTimeMultiplayer', new THREE.Float32BufferAttribute(timeMultiplyerArray, 1))
+
+    
 
 
     texture.flipY = false
@@ -160,25 +167,76 @@ function createFireWork(count, size, position, texture, radius, color){
 
 }
 
-createFireWork( 
-    100,  //count
-    1.,  //size 
-    new THREE.Vector3(),  //position
-    textures[7],  //texture
-    1, // radius,
-    new THREE.Color('#8affff')  // color
-) 
+createRandomFireWork()
 
-window.addEventListener( 'click', ()=> {
-    createFireWork( 
-        100,  //count
-        1.,  //size 
-        new THREE.Vector3(),  //position
-        textures[7],  //texture
-        1, // radius,
-        new THREE.Color('#8affff')  // color
-    )  
-})
+function createRandomFireWork(){
+    const count = Math.round( 400 + Math.random() * 1000 )
+    const positon = new THREE.Vector3(
+        (Math.random() - .5) * 2,
+        Math.random() ,
+        (Math.random() - .5) * 2,
+    )
+    const size = 0.1 + Math.random() * .1
+    const texture = textures[ Math.floor( Math.random() * textures.length )]
+    const color = new THREE.Color().setHSL( Math.random(), 1, 0.7)
+    const radius = .5 + Math.random()
+
+    createFireWork( count, size, positon, texture, radius, color)
+
+}
+
+
+window.addEventListener( 'click', createRandomFireWork )
+
+
+const sky = new Sky();
+sky.scale.setScalar( 450000 );
+scene.add( sky );
+
+const sun = new THREE.Vector3();
+
+/// GUI
+
+const effectController = {
+    turbidity: 10,
+    rayleigh: 3,
+    mieCoefficient: 0.005,
+    mieDirectionalG: 0.7,
+    elevation: 2,
+    azimuth: 180,
+    exposure: renderer.toneMappingExposure
+};
+
+function guiChanged() {
+
+    const uniforms = sky.material.uniforms;
+    uniforms[ 'turbidity' ].value = effectController.turbidity;
+    uniforms[ 'rayleigh' ].value = effectController.rayleigh;
+    uniforms[ 'mieCoefficient' ].value = effectController.mieCoefficient;
+    uniforms[ 'mieDirectionalG' ].value = effectController.mieDirectionalG;
+
+    const phi = THREE.MathUtils.degToRad( 90 - effectController.elevation );
+    const theta = THREE.MathUtils.degToRad( effectController.azimuth );
+
+    sun.setFromSphericalCoords( 1, phi, theta );
+
+    uniforms[ 'sunPosition' ].value.copy( sun );
+
+    renderer.toneMappingExposure = effectController.exposure;
+    renderer.render( scene, camera );
+
+}
+
+
+gui.add( effectController, 'turbidity', 0.0, 20.0, 0.1 ).onChange( guiChanged );
+gui.add( effectController, 'rayleigh', 0.0, 4, 0.001 ).onChange( guiChanged );
+gui.add( effectController, 'mieCoefficient', 0.0, 0.1, 0.001 ).onChange( guiChanged );
+gui.add( effectController, 'mieDirectionalG', 0.0, 1, 0.001 ).onChange( guiChanged );
+gui.add( effectController, 'elevation', 0, 90, 0.1 ).onChange( guiChanged );
+gui.add( effectController, 'azimuth', - 180, 180, 0.1 ).onChange( guiChanged );
+gui.add( effectController, 'exposure', 0, 1, 0.0001 ).onChange( guiChanged );
+
+guiChanged()
 
 
 // plane
--- shaders/firework/vertex.glsl
+++ shaders/firework/vertex.glsl
@@ -4,6 +4,7 @@ uniform float uSize;
 uniform float uProgress;
 
 attribute float aSizes;
+attribute float aTimeMultiplayer;
 
 float remap(float value, float originMin, float originMax, float destinationMin, float destinationMax)
 {
@@ -11,22 +12,33 @@ float remap(float value, float originMin, float originMax, float destinationMin,
 }
 
 void main(){
-
+    float progress = uProgress * aTimeMultiplayer;
     vec3 newPostion = position;
 
     // explosion
-    float explostionProgress = remap( uProgress, .0, .1, .0, 1. );
+    float explostionProgress = remap( progress, .0, .1, .0, 1. );
     explostionProgress = clamp(explostionProgress, .0, 1.);
     explostionProgress = 1. - pow( 1. - explostionProgress, 3.);
     newPostion *= explostionProgress;
 
     // falling
-    float fallingProgress = remap(uProgress, 0.1, 1.0, 0., 1.);
+    float fallingProgress = remap(progress, 0.1, 1.0, 0., 1.);
     fallingProgress = clamp( fallingProgress, .0, 1.);
     fallingProgress = 1. - pow(1. -fallingProgress,  3.);
     newPostion.y -= fallingProgress * 0.2;
 
     // scaling
+    float sizeOpeningProgress = remap( progress, .0, .125, .0, 1.);
+    float sizeClosingProgress = remap( progress, .125, 1., 1., 0.);
+    float sizeProgress = min( sizeClosingProgress, sizeOpeningProgress);
+    sizeProgress = clamp(sizeProgress, 0., 1.);
+
+    // twinkling
+    float twinklingProgress = remap( progress, .2, .8, 0., 1.) ;
+    twinklingProgress = clamp( twinklingProgress, 0., 1.);
+    float sizeTwinkling = sin( progress * 30.) * .5 + .5 ;
+    sizeTwinkling = 1. - sizeTwinkling * twinklingProgress;
+
 
     // Matrix
     vec4 modelPosition = modelMatrix * vec4(newPostion, 1.);
@@ -35,6 +47,12 @@ void main(){
     
     // final position
     gl_Position =  projectionPosition;
-    gl_PointSize = uSize * uResolution.y * aSizes ;
+
+    // final size
+    gl_PointSize = uSize * uResolution.y * aSizes * sizeProgress * sizeTwinkling;
     gl_PointSize *= 1. / - viewPosition.z ;
+    
+    if ( gl_PointSize < 1.){
+        gl_Position = vec4( 9999.9);
+    }
 }
